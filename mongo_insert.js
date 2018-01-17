f4963c00cd13f8bc85e06a80fa9fe3cce2a0b567
cjj993 <cjj9931207@gmail.com>
2018-01-17 10:58:11 +0800

    console.dir

--- mongo_insert.js
+++ mongo_insert.js
@@ -1,7 +1,7 @@
 var MongoClient = require("mongodb").MongoClient;
 var DB_URL = "mongodb://localhost:27017";
 
-MongoClient.connect(DB_URL,
+MongoClient.connect(DB_URL,poolSize=5,
                     function(err, database) {
                         if(err) { return console.dir(err); }
                         console.log('连接成功!');
@@ -9,9 +9,14 @@ MongoClient.connect(DB_URL,
                         var tt=myAwesomeDB.collection('foo');
                         tt.insertOne({
                             bar: "123"
-                        })
-                            .then(function(result){
-                                console.log(result);
-                            });
-                        return 1;
+                        },
+						function(err,result){
+							if(err)
+								{ return console.dir(err); }//console.log()，会在浏览器控制台打印出信息。
+															//console.dir()可以显示一个对象所有的属性和方法
+							console.log(result);
+							return 1;
+						}
+						
+						)
                     });
