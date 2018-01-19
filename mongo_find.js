33f5cc058c077f707f5533a5ebaf580b2888a30a
chenjiajun1207 <cjj9931207@gmail.com>
2018-01-19 13:36:58 +0800

    test

--- mongo_find.js
+++ mongo_find.js
@@ -1,17 +1,15 @@
-var mongodb = require('mongodb') 
 var MongoClient = require('mongodb').MongoClient;
 var DB_CONN_STR = 'mongodb://localhost:27017';
 
-var selectData = function(db, callback) {
+var selectData = function(database, callback) {
     const myAwesomeDB = database.db('test');
-	
+
     var tt=myAwesomeDB.collection('foo');
     //查询数据
     var whereStr = {
         "name": 'node'
     };
-    collection.find(whereStr,
-    function(error, cursor) {
+    tt.find(whereStr,function(error, cursor) {
         cursor.each(function(error, doc) {
             if (doc) {
                 //console.log(doc);
@@ -21,14 +19,14 @@ var selectData = function(db, callback) {
             }
         });
     });
-}
+};
 
 MongoClient.connect(DB_CONN_STR,
-function(err, database) {
-    console.log("连接成功！");
-    selectData(db,
-    function(result) {
-        console.log(result);
-        db.close();
-    });
-});
+                    function(err, database) {
+                        console.log("连接成功！");
+                        selectData(database,
+                                   function(result) {
+                                       console.log(result);
+                                       db.close();
+                                   });
+                    });
